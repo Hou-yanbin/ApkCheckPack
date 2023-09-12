8849dcb1127a900431b18ff11a8423355ad6e24b
Jack hou <58129352+Hou-yanbin@users.noreply.github.com>
2023-09-12 13:51:08 +0800

    Add files via upload

--- ApkCheckPack.py
+++ ApkCheckPack.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # coding=utf-8
 # python version 3.7 by 6time
-#
+#Mac版本ApkCheckPack工具@Jackhou
 
 import zipfile, hashlib, json
 from tkinter import *
-from tkinter.filedialog import askdirectory, askopenfilename
 from tkinter.messagebox import showinfo
-import windnd
+import tkinter.filedialog as filedialog
 
 # packs = """"""
 # # 字符串方式加载特征
@@ -19,35 +18,27 @@ with open('apkpack.json', 'r', encoding='utf-8') as f:
     markNameMap = dict(markNameMap)
     pass
 
-
 def check_jiagu(filename):
     azip = zipfile.ZipFile(filename)  # 默认模式r,读
-    # print(azip.namelist()) # 返回所有文件夹和文件
-    # print(azip.filename)# 返回该zip的文件名
     jigu = u''
     for zippath in azip.namelist():
         if 'lib' in zippath or 'assets' in zippath:
-            # print(zippath)
             for key, value in markNameMap.items():
                 for mark in value:
                     if mark in zippath:
                         print(u"检测到 【{}】 加固\n匹配特征:{}->{}\n".format(key, zippath, mark))
                         jigu += (u"检测到 【{}】 加固\n匹配特征:{}->{}\n".format(key, zippath, mark))
-                        # return (u"检测到 【{}】 加固\n匹配特征:{}\n->{}\n".format(key, zippath, mark))
     if len(jigu) > 0:
         return jigu
-    # so库文件模式找不到就全量匹配
     for zippath in azip.namelist():
         for key, value in markNameMap.items():
             for mark in value:
                 if mark in zippath:
                     print(u"检测到 【{}】 加固\n匹配特征:{}->{}\n".format(key, zippath, mark))
-                    # return (u"【全量匹配】检测到 【{}】 加固\n匹配特征:{}\n->{}\n".format(key, zippath, mark))
                     jigu += (u"检测到 【{}】 加固\n匹配特征:{}->{}\n".format(key, zippath, mark))
     if len(jigu) > 0:
         return jigu
     return (u"未检测到加固")
-
 
 def md5sum(file_name):
     with open(file_name, 'rb') as fp:
@@ -56,52 +47,44 @@ def md5sum(file_name):
     print(file_md5)
     pathmd5.set(file_md5)
 
-
 def selectfilePath():
-    path_ = askopenfilename()
+    path_ = filedialog.askopenfilename(filetypes=[("APK files", "*.apk")])
     path.set(path_)
-    if path_[-4:] != '.apk':
-        pathjiagu.set("非APK文件！")
-    else:
-        _jiagu = check_jiagu(path_)
-        pathjiagu.set(_jiagu)
-        md5sum(path_)
+    if path_:
+        if path_[-4:] != '.apk':
+            pathjiagu.set("非APK文件！")
+        else:
+            _jiagu = check_jiagu(path_)
+            pathjiagu.set(_jiagu)
+            md5sum(path_)
+            update_result()  # 选择文件后立即更新结果
 
-
-def dragged_files(files):
-    msg = '\n'.join((item.decode('gbk') for item in files))
-    print(msg)
-    path.set(msg)
-    if msg[-4:] != '.apk':
-        pathjiagu.set("非APK文件！")
-    else:
-        _jiagu = check_jiagu(msg)
-        pathjiagu.set(_jiagu)
-        md5sum(msg)
-
+def update_result():
+    if path.get():
+        if path.get()[-4:] == '.apk':
+            _jiagu = check_jiagu(path.get())
+            pathjiagu.set(_jiagu)
+            md5sum(path.get())
+    root.update_idletasks()  # 刷新界面
 
 if __name__ == '__main__':
-    help = ("""ApkCheckPack v1.2 by 6time\n
+    help = ("""ApkCheckPack for Mac v1.2 by Jackhou\n
     目前规则总数：177条
         """)
     root = Tk()
-    root.title("ApkCheckPack v1.2 by 6time")
-    # 获取屏幕 宽、高
+    root.title("ApkCheckPack for Mac v1.2 by Jackhou")
     ws = root.winfo_screenwidth()
     hs = root.winfo_screenheight()
     root.geometry('%dx%d+%d+%d' % (600, 500, ws / 2 - 300, 0))
-    root.resizable(True, True)  # 窗口大小可变
+    root.resizable(True, True)
     path = StringVar()
     pathjiagu = StringVar()
     pathmd5 = StringVar()
     Label(root, text="APK文件路径").pack(fill=BOTH)
     Entry(root, textvariable=path).pack(fill=BOTH)
-
-    windnd.hook_dropfiles(root, func=dragged_files)
-    # Button(root, text="路径选择", command=selectfilePath).pack(fill=BOTH)
-
     Label(root, text="文件MD5值").pack(fill=BOTH)
     Label(root, textvariable=pathmd5).pack(fill=BOTH)
     Label(root, text="检测结果").pack(fill=BOTH)
     Label(root, textvariable=pathjiagu).pack(fill=BOTH)
+    Button(root, text="路径选择", command=selectfilePath).pack(fill=BOTH)
     root.mainloop()
